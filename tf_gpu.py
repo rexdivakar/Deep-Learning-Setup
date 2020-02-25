4814d360c5ab37d82e3711af518d83c9a6a0b615
rexdivakar <rexdivakar@hotmail.com>
2020-02-25 21:08:15 +0530

    final update

--- tf_gpu.py
+++ tf_gpu.py
@@ -1,3 +1,11 @@
 tf.test.is_gpu_available(
     cuda_only=False, min_cuda_compute_capability=None
 )
+
+
+
+import tensorflow as tf
+if tf.test.gpu_device_name():
+    print('Default GPU Device: {}'.format(tf.test.gpu_device_name()))
+else:
+    print("Please install GPU version of TF")
