d9264ed034b7366448ec6bca008472c644b692e9
rexdivakar <rexdivakar@hotmail.com>
2020-07-04 14:26:43 +0530

    Updatest test

--- gpu_speed_test.py
+++ gpu_speed_test.py
@@ -1,26 +1,8 @@
-# %%
 import tensorflow as tf
-with tf.device('/gpu:0'):
-    a = tf.constant([1.0, 2.0, 3.0, 4.0, 5.0, 6.0], shape=[2, 3], name='a')
-    b = tf.constant([1.0, 2.0, 3.0, 4.0, 5.0, 6.0], shape=[3, 2], name='b')
-    c = tf.matmul(a, b)
 
-with tf.Session() as sess:
-    print (sess.run(c))
-
-# %%
-import tensorflow as tf
-with tf.device('/cpu:0'):
-    a = tf.constant([1.0, 2.0, 3.0, 4.0, 5.0, 6.0], shape=[2, 3], name='a')
-    b = tf.constant([1.0, 2.0, 3.0, 4.0, 5.0, 6.0], shape=[3, 2], name='b')
-    c = tf.matmul(a, b)
-
-with tf.Session() as sess:
-    print (sess.run(c))
-
-# %%
-import tensorflow as tf
-if tf.test.gpu_device_name():
-    print('Default GPU Device: {}'.format(tf.test.gpu_device_name()))
-else:
-    print("Please install GPU version of TF")
+# Device Name
+print('Device Name: '+tf.test.gpu_device_name())
+# Version-check
+print('Version: '+tf.__version__)
+# CUDA Support
+print('CUDA Support: '+str(tf.test.is_built_with_cuda()))
